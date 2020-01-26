be7689a5913d7b343a467db8ca408b7a18f564ae
Anton Kozyriev <sonara4mus@gmail.com>
2020-01-26 21:35:34 +0200

    Domain: Change Product model from INT to GTIN string

--- src/OnionDI/OnionDI.Domain/Models/OrderProduct.cs
+++ src/OnionDI/OnionDI.Domain/Models/OrderProduct.cs
@@ -5,7 +5,7 @@ namespace OnionDI.Domain.Models
         public int OrderId { get; set; }
         public Order Order { get; set; }
 
-        public string ProductId { get; set; }
+        public string ProductGtin { get; set; }
         public Product Product { get; set; }
 
         public int ProductCount { get; set; }
--- src/OnionDI/OnionDI.Domain/Models/Product.cs
+++ src/OnionDI/OnionDI.Domain/Models/Product.cs
@@ -2,7 +2,7 @@ namespace OnionDI.Domain.Models
 {
     public class Product
     {
-        public int Id { get; set; }
+        public string Gtin { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
         public decimal Price { get; set; }
