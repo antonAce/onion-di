94de11835fd97a9548ee3e9d28d3acdf03f90329
Anton Kozyriev <sonara4mus@gmail.com>
2020-02-03 01:10:03 +0200

    Data: apply entities configurations

--- src/OnionDI/OnionDI.Data/EF/Context/DataContext.cs
+++ src/OnionDI/OnionDI.Data/EF/Context/DataContext.cs
@@ -2,19 +2,22 @@ using Microsoft.EntityFrameworkCore;
 
 using OnionDI.Domain.Models;
 
+using OnionDI.Data.EF.Config.Mapping;
+
 namespace OnionDI.Data.EF.Context
 {
     public class DataContext : DbContext
     {
         public DbSet<Order> Orders { get; set; }
         public DbSet<Product> Products { get; set; }
-        public DbSet<OrderProduct> OrderProducts { get; set; }
 
         public DataContext(DbContextOptions<DataContext> options) : base(options) {}
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            base.OnModelCreating(modelBuilder);
+            modelBuilder.ApplyConfiguration(new OrdersMapping());
+            modelBuilder.ApplyConfiguration(new OrderProductMapping());
+            modelBuilder.ApplyConfiguration(new ProductMapping());
         }
     }
 }
