7c049cf11d69a2f09ea7b06a7023437b34d1086b
Anton Kozyriev <sonara4mus@gmail.com>
2020-02-09 02:05:11 +0200

    API: Implementing product controller

--- src/OnionDI/OnionDI/Controllers/ProductController.cs
+++ src/OnionDI/OnionDI/Controllers/ProductController.cs
@@ -5,21 +5,104 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 
+using OnionDI.Business.Infrastructure.Interfaces;
+using OnionDI.Business.Infrastructure.DTO;
+using OnionDI.Business.Services;
+
+using OnionDI.Models;
+
 namespace OnionDI.Controllers
 {
     [ApiController]
     [Route("api/[controller]")]
     public class ProductController : ControllerBase
     {
-        public ProductController()
+        private readonly IProductService _productService;
+
+        public ProductController(IProductService productService)
         {
-            
+            _productService = productService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetProducts(
+            [FromQuery(Name = "from")] int? from,
+            [FromQuery(Name = "to")] int? to)
+        {
+            try
+            {
+                int? limit = (to.HasValue && from.HasValue) ? (to - from) : null;
+                var products = await _productService
+                    .ListProducts(limit, from)
+                    .ToArrayAsync();
+                return Ok(products);
+            }
+            catch (Exception e)
+            {
+                return StatusCode(500, "Oops, something went wrong!");
+            }
         }
         
-        [HttpGet]
-        public IActionResult GetProducts()
+        [HttpGet("{gtin}")]
+        public async Task<IActionResult> GetProducts(string gtin)
         {
-            return Ok("");
+            try
+            {
+                var product = await _productService.GetProductByGtin(gtin);
+                return Ok(product);
+            }
+            catch (Exception e)
+            {
+                return StatusCode(500, "Oops, something went wrong!");
+            }
+        }
+        
+        [HttpPost]
+        public async Task<IActionResult> AddProduct([FromBody] ProductDto product)
+        {
+            try
+            {
+                await _productService.CreateProduct(product);
+                return Ok("New product was successfully added!");
+            }
+            catch (Exception e)
+            {
+                return StatusCode(500, "Oops, something went wrong!");
+            }
+        }
+
+        [HttpPut("{gtin}")]
+        public async Task<IActionResult> ModifyProduct(string gtin, [FromBody] ProductToModify product)
+        {
+            try
+            {
+                await _productService.ModifyProduct(new ProductDto
+                {
+                    Gtin = gtin,
+                    Name = product.Name,
+                    Description = product.Description,
+                    Price = product.Price
+                });
+                return Ok("Product was successfully modified!");
+            }
+            catch (Exception e)
+            {
+                return StatusCode(500, "Oops, something went wrong!");
+            }
+        }
+        
+        [HttpDelete("{gtin}")]
+        public async Task<IActionResult> DeleteProduct(string gtin)
+        {
+            try
+            {
+                await _productService.RemoveProduct(new ProductDto { Gtin = gtin });
+                return Ok("Product was successfully removed!");
+            }
+            catch (Exception e)
+            {
+                return StatusCode(500, "Oops, something went wrong!");
+            }
         }
     }
 }
---
+++ src/OnionDI/OnionDI/Models/ProductToModify.cs
@@ -0,0 +1,9 @@
+namespace OnionDI.Models
+{
+    public class ProductToModify
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+    }
+}
